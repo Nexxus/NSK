3e87359360c08fd75df3ee63a421ef3c0580caed
nebbii <ben.wolthuis@gmail.com>
2017-03-07 23:55:46 +0100

    Base done, homepage and track & trace module setup manual breadcrumb

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -10,10 +10,20 @@ class Builder implements ContainerAwareInterface
 {
     use ContainerAwareTrait;
     
-    public function mainMenu(FactoryInterface $factory, array $options)
+    public function createMainMenu(FactoryInterface $factory, array $options)
     {
         $menu = $factory->createItem('root');
         
+        $menu->setChildrenAttribute('class', 'nav navbar-nav');
+        
+        $menu->addChild("<img height=24 src='images/logos/header-logoGlow.png' >",
+          array(
+            'uri' => '/',
+            'extras' => array(
+              'safe_label' => true
+            )
+          ));
+
         $menu->addChild('Home', array('route' => 'home'));
         $menu->addChild('Track & Trace', array('route' => 'tnt'));
         
--- src/TnTBundle/Controller/DefaultController.php
+++ src/TnTBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace TnTBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Appbundle\Builder;
 
 class DefaultController extends Controller
 {   
@@ -12,6 +13,9 @@ class DefaultController extends Controller
      */
     public function TnTAction()
     {
+        //$builder = $this->get('app.menubuilder');
+        //$crumb = $builder->createBreadcrumb();
+        
         return $this->render('TnTBundle:Default:index.html.twig');
     }
 }
