62f58da4b78b3e51f1c6317d6e254f4fc2dd51d9
Ben Wolthuis <ben.wolthuis@gmail.com>
2017-03-23 17:12:38 +0100

    added brand entity; started translating

---
+++ src/TrackBundle/Entity/Brand.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace TrackBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Brand
+ *
+ * @ORM\Table(name="brand")
+ * @ORM\Entity(repositoryClass="TrackBundle\Repository\BrandRepository")
+ */
+class Brand
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Brand
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
