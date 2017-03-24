d12aa5a656e9dd7ac2fdf5a24d49aa870c454a9f
Ben Wolthuis <ben.wolthuis@gmail.com>
2017-03-24 13:15:45 +0100

    admin: initialized location crud

---
+++ src/TrackBundle/Controller/LocationController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace TrackBundle\Controller;
+
+use TrackBundle\Entity\Location;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Location controller.
+ *
+ * @Route("admin/location")
+ */
+class LocationController extends Controller
+{
+    /**
+     * Lists all location entities.
+     *
+     * @Route("/", name="admin_location_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $locations = $em->getRepository('TrackBundle:Location')->findAll();
+
+        return $this->render('location/index.html.twig', array(
+            'locations' => $locations,
+        ));
+    }
+
+    /**
+     * Creates a new location entity.
+     *
+     * @Route("/new", name="admin_location_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $location = new Location();
+        $form = $this->createForm('TrackBundle\Form\LocationType', $location);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($location);
+            $em->flush($location);
+
+            return $this->redirectToRoute('admin_location_show', array('id' => $location->getId()));
+        }
+
+        return $this->render('location/new.html.twig', array(
+            'location' => $location,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a location entity.
+     *
+     * @Route("/{id}", name="admin_location_show")
+     * @Method("GET")
+     */
+    public function showAction(Location $location)
+    {
+        $deleteForm = $this->createDeleteForm($location);
+
+        return $this->render('location/show.html.twig', array(
+            'location' => $location,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing location entity.
+     *
+     * @Route("/{id}/edit", name="admin_location_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Location $location)
+    {
+        $deleteForm = $this->createDeleteForm($location);
+        $editForm = $this->createForm('TrackBundle\Form\LocationType', $location);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin_location_edit', array('id' => $location->getId()));
+        }
+
+        return $this->render('location/edit.html.twig', array(
+            'location' => $location,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a location entity.
+     *
+     * @Route("/{id}", name="admin_location_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Location $location)
+    {
+        $form = $this->createDeleteForm($location);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($location);
+            $em->flush($location);
+        }
+
+        return $this->redirectToRoute('admin_location_index');
+    }
+
+    /**
+     * Creates a form to delete a location entity.
+     *
+     * @param Location $location The location entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Location $location)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('admin_location_delete', array('id' => $location->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
