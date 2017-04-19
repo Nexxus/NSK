8709aeca5f67820bdd16c83bb89511ea8dda497d
Ben Ben <Ben Ben>
2017-04-19 17:40:58 +0200

    start on product status controller!

--- src/TrackBundle/Controller/ProductStatusController.php
+++ src/TrackBundle/Controller/ProductStatusController.php
@@ -2,8 +2,13 @@
 
 namespace TrackBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use TrackBundle\Entity\ProductStatus;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;	
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
 
 /**
  * @Route("/admin/status")
@@ -21,6 +26,26 @@ class ProductStatusController extends Controller
         
         return $this->render('admin/status/index.html.twig', array(
             'productstatus' => $productstatus)); 
+    }
+    
+    /**
+     * @Route("/create", name="status_create")
+     */
+    public function createAction()
+    {
+        $status = new ProductStatus();
+        $status->setName("Product Status Name");
+        $status->setPindex(1);
+        
+        $form = $this->createFormBuilder($status)
+                ->add('name', TextType::class)
+                ->add('pindex', IntegerType::class)
+                ->add('save', SubmitType::class, array('label' => 'Create Status'))
+                ->getForm();
+        
+        return $this->render('admin/status/new.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
     
     /**
