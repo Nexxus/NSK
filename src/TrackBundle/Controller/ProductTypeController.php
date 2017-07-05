21edfc4add150eec600ca13e46e8550102f95fee
nebbii <ben.wolthuis@gmail.com>
2017-07-05 15:12:37 +0200

    product type index made, start on create function

--- src/TrackBundle/Controller/ProductTypeController.php
+++ src/TrackBundle/Controller/ProductTypeController.php
@@ -4,11 +4,31 @@ namespace TrackBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-
+use Doctrine\ORM\EntityManagerInterface;
 
 /**
  * @Route("admin/type")
  */
 class ProductTypeController extends Controller
 {
+    /**
+     * @Route("/", name="producttype_index") 
+     */
+    public function indexAction() 
+    {
+        $em = $this->getDoctrine();
+        $products = $em->getRepository('TrackBundle:ProductType')
+                        ->findAll();
+        
+        return $this->render('admin/type/index.html.twig', 
+                array('products' => $products));
+    }
+    
+    /**
+     * @Route("/create", name="producttype_new")
+     */
+    public function createAction()
+    {
+        
+    }
 }
