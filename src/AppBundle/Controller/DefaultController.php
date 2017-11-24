083c211d72290ed463e70a4458ae79509daaa31e
nebbii <ben.wolthuis@gmail.com>
2017-11-24 15:07:49 +0100

    login: Basic FOS user login & logout works

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,13 +4,8 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-use AppBundle\Entity\User;
 
 class DefaultController extends Controller
 {
@@ -20,26 +15,6 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         return $this->render('base.html.twig');
-    }
+    }  
     
-    /**
-     * @Route("/logintest1", name="login_old")
-     */
-    public function loginAction(Request $request)
-    {
-        // get auth utils object
-        $authUtils = $this->get('security.authentication_utils');
-        
-        $error = $authUtils->getLastAuthenticationError();
-        
-        // last username entered by the user
-        $lastUsername = $authUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', array(
-            "last_username" => $lastUsername,
-            "error" => $error,
-        ));
-        
-        
-    }
 }
