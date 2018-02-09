e2e2e2053f05a6d1be3214700c15c85898ec8762
Ben Wolthuis <ben.wolthuis@gmail.com>
2018-02-09 14:18:18 +0100

    task: imports product on id

--- src/TrackBundle/Controller/ProductTaskController.php
+++ src/TrackBundle/Controller/ProductTaskController.php
@@ -4,17 +4,26 @@ namespace TrackBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+use TrackBundle\Entity\Product;
 
 /**
- * @Route("track/checklist")
+ * @Route("track/task")
  */
 class ProductTaskController extends Controller
 {
     
     /**
-     * @Route("show/{id}", name="track_checklist_show")
+     * @Route("/show/{id}", name="track_checklist_show")
+     * @Method({"GET"})
      */
-    public function showListAction($id) {
-        return $this->render("product/checklist/list.html.twig");
+    public function showListAction(Product $product) {
+        $em = $this->getDoctrine()->getManager();
+        
+        return $this->render("product/checklist/list.html.twig", [
+            'product' => $product,
+            'tasks' => [],
+        ]);
     }
 }
