9e1348c6aa3e429bceeec0b6507e6cfc449d58e5
Ben Wolthuis <ben.wolthuis@gmail.com>
2018-02-12 12:50:18 +0100

    track: fixed andWhere clause

--- src/TrackBundle/Controller/ProductController.php
+++ src/TrackBundle/Controller/ProductController.php
@@ -70,13 +70,13 @@ class ProductController extends Controller
         {
             $searched = true;
             // location
-            if(isset($search_query['spec']['location'])) {
+            if(isset($search_query['spec']['location']) && $search_query['spec']['location']!=null) {
                 $productquery->andWhere('p.location = :q')
                         ->setParameter('q', $search_query['spec']['location']);
             }
             
             // type
-            if(isset($search_query['spec']['type'])) {
+            if(isset($search_query['spec']['type']) && $search_query['spec']['type']!=null) {
                 $productquery->andWhere('p.type = :q')
                         ->setParameter('q', $search_query['spec']['type']);
             }
