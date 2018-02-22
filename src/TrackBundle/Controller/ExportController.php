282cb52ca8c8aab1b407dae6864558ee86b6caee
Ben Wolthuis <ben.wolthuis@gmail.com>
2018-02-22 12:08:23 +0100

    Track: generated ExportController

---
+++ src/TrackBundle/Controller/ExportController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace TrackBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class ExportController extends Controller
+{
+}
