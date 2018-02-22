cd06f40ece46112d99695ae05d2e86ad6d518489
Ben Wolthuis <ben.wolthuis@gmail.com>
2018-02-22 13:02:12 +0100

    export: created queryAction()

--- src/TrackBundle/Controller/ExportController.php
+++ src/TrackBundle/Controller/ExportController.php
@@ -3,8 +3,28 @@
 namespace TrackBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
+/**
+ * Controller for exporting CSVs
+ */
 class ExportController extends Controller
 {
+    
+    /**
+     * @Route("/track/exportquery")
+     * @Method("GET")
+     */
+    public function exportQueryAction() {
+        $em = $this->getDoctrine()->getManager();
+        
+        $response = new Response(
+                "Test",
+                Response::HTTP_OK,
+                array('content-type' => 'text/html'));
+        
+        return $response;
+    }
 }
