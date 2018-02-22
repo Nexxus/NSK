02be3cf4df8cbfa806a7ee3d5064aa1a6de43c85
Ben Wolthuis <ben.wolthuis@gmail.com>
2018-02-22 13:44:37 +0100

    export: products successfully retrieved

--- src/TrackBundle/Controller/ExportController.php
+++ src/TrackBundle/Controller/ExportController.php
@@ -3,6 +3,7 @@
 namespace TrackBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -19,11 +20,29 @@ class ExportController extends Controller
      * @Route("/track/export/csv", name="track_export_csv")
      * @Method("GET")
      */
-    public function exportCSVAction() {
+    public function exportCSVAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
         
+        $selection = $request->query->get('id');
+        
+        // get items
+        $query = $em->getRepository("TrackBundle:Product")->createQueryBuilder('p');
+        
+        foreach($selection as $id) {
+            $query->orWhere("p.id = {$id}");
+        }
+        
+        $products = $query->getQuery()->getResult();
+        
+        // serialize into text
+        $serialize = "";
+        
+        foreach($products as $product) {
+
+        }
+        
         $response = new Response(
-                "Test",
+                $serialize,
                 Response::HTTP_OK,
                 array('content-type' => 'text/html'));
         
