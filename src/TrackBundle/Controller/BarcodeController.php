400d4b07778601a6e3def7850e31c66fafcc9e85
nebbii <ben.wolthuis@gmail.com>
2018-06-21 15:27:48 +0200

    new environment imported; work on a4 barcode print

--- src/TrackBundle/Controller/BarcodeController.php
+++ src/TrackBundle/Controller/BarcodeController.php
@@ -13,7 +13,7 @@ class BarcodeController extends Controller
     /**
      * Prints a barcode PDF page
      * 
-     * @Route("print/barcode39/{sku}", name="track_print_barcode")
+     * @Route("print/barcode/single/{sku}", name="track_print_barcode")
      */
     public function printAction($sku)
     {
@@ -73,9 +73,75 @@ class BarcodeController extends Controller
      * margin footer
      * L - landscape, P - portrait
      * 
-     * @Route("print/barcode39multi", name="track_print_barcode_multi")
+     * @Route("print/barcode/multi", name="track_print_barcode_multi")
      */
     public function printMultiAction(Request $request) {
+        
+        $mpdfService = $this->get('tfox.mpdfport');
+        
+        
+        // needs to change page size, turn off default arguments
+        $mpdfService->setAddDefaultConstructorArgs(false);
+        
+        
+        $mpdf = $mpdfService->getMpdf(['', [54,25] ,'0','',0,0,0,0,0,0,'P']);
+        
+        $mpdf->setTitle("ERCPv2 Barcode Generator");
+
+        // retrieve ids
+        $ids = $_GET['id'];
+        
+        $repo = $this->getDoctrine()->getRepository('TrackBundle:Product');
+        
+        $items = $repo->findBy([
+            'id' => $ids
+        ]);
+        
+        // print barcodes
+	foreach($items as $row) {
+            $sku = $row->getSku();
+            $mpdf->AddPage();
+            $html = '<html>
+                        <head><META HTTP-EQUIV="Window-target" CONTENT="_blank">
+                        <style>
+                            body {font-family: sans-serif; font-size: 9pt;}
+                            table {border-collapse: collapse; border: 0; }
+                            .barcode { padding: 0.5mm; margin: 0; vertical-align: top; color: #000000; }
+                            .barcodecell { text-align: center; vertical-align: top; padding: 0; }
+                        </style>
+                        </head>
+                        <body>
+                            <table class="items" cellpadding="0" border="0";>
+                                <tr>
+                                    <td style="text-align:center;"><b>Copiatek</b></td>
+                                </tr>
+                                <tr>
+                                    <td class="barcodecell">
+                                        <barcode code="'.$sku.'" type="C39" class="barcode" size=1 height=1.1/>
+                                        &nbsp;
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align:center;"><b>*&nbsp;'.$sku.'&nbsp;*</b></td>
+                                </tr>
+                            </table>
+                        </body>
+                    </html>';
+
+            $mpdf->writeHTML($html);
+            
+        }
+        
+        return new Response($mpdf->Output());
+        
+    }
+    
+    /**
+     * Prints a barcode PDF page
+     * 
+     * @Route("print/barcode/multi/a4", name="track_print_barcode_a4")
+     */
+    public function printMultiA4(Request $request) {
         
         $mpdfService = $this->get('tfox.mpdfport');
         
