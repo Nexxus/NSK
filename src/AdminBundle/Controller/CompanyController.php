9e36127ab5b8f2d86aafba01b3a79d2112067549
Ben Wolthuis <ben.wolthuis@copiatek.nl>
2018-09-04 14:25:28 +0200

    admin: line spacing

--- src/AdminBundle/Controller/CompanyController.php
+++ src/AdminBundle/Controller/CompanyController.php
@@ -5,7 +5,8 @@ namespace AdminBundle\Controller;
 use AdminBundle\Entity\Company;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Company controller.
