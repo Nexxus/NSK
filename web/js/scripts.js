8664d1725bc7eb62d7893e0bf257042090811116
Jorrit Steetskamp <jorrit@rezept.nl>
2018-10-05 12:20:40 +0200

    #85 scan, type, autogeneration of orderNr and sku

---
+++ web/js/scripts.js
@@ -0,0 +1,12 @@
+﻿
+/* focusBarcodeInput */
+
+$(document).ready(function () {
+    focusBarcodeInput();
+});
+
+function focusBarcodeInput() {
+    if (!$('input.focus:first').val()) {
+        $('input.focus:first').focus();
+    }
+}
