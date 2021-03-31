cb53359080dc2a75f0d77ee22f421077123edc60
Jorr.it <jorrit@jorr.it>
2021-03-31 11:24:24 +0200

    fixes

--- src/AppBundle/Controller/PurchaseOrderController.php
+++ src/AppBundle/Controller/PurchaseOrderController.php
@@ -296,9 +296,40 @@ class PurchaseOrderController extends Controller
         $em = $this->getDoctrine()->getManager();
         $order = $em->getRepository('AppBundle:PurchaseOrder')->find($id);
 
-        $html = $this->render('AppBundle:PurchaseOrder:print.html.twig', array('order' => $order));
+        $supplierBarcode = "";
+        if ($order->getSupplier() && strlen($order->getSupplier()->getName()) > 20)
+            $supplierBarcode = substr($this->cleanString($order->getSupplier()->getName(), 0, 20));
+        elseif ($order->getSupplier())
+            $supplierBarcode = $this->cleanString($order->getSupplier()->getName());
+
+        $html = $this->render('AppBundle:PurchaseOrder:print.html.twig', array('order' => $order, 'supplierBarcode' => $supplierBarcode));
         $mPdfConfiguration = ['', 'A4' ,'','',10,10,10,10,0,0,'P'];
 
         return $this->getPdfResponse("Nexxus purchase order", $html, $mPdfConfiguration);
+    }
+
+    private function cleanString($text) {
+        $utf8 = array(
+            '/[áàâãªä]/u'   =>   'a',
+            '/[ÁÀÂÃÄ]/u'    =>   'A',
+            '/[ÍÌÎÏ]/u'     =>   'I',
+            '/[íìîï]/u'     =>   'i',
+            '/[éèêë]/u'     =>   'e',
+            '/[ÉÈÊË]/u'     =>   'E',
+            '/[óòôõºö]/u'   =>   'o',
+            '/[ÓÒÔÕÖ]/u'    =>   'O',
+            '/[úùûü]/u'     =>   'u',
+            '/[ÚÙÛÜ]/u'     =>   'U',
+            '/ç/'           =>   'c',
+            '/Ç/'           =>   'C',
+            '/ñ/'           =>   'n',
+            '/Ñ/'           =>   'N',
+            '/–/'           =>   '-', // UTF-8 hyphen to "normal" hyphen
+            '/[’‘‹›‚]/u'    =>   ' ', // Literally a single quote
+            '/[“”«»„]/u'    =>   ' ', // Double quote
+            '/ /'           =>   ' ', // nonbreaking space (equiv. to 0x160)
+            '/&/'           =>   'en', 
+        );
+        return preg_replace(array_keys($utf8), array_values($utf8), $text);
     }
 }
